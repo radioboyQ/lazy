09deb81d617f16cc010aad5a0eb9da8890442340
Scott.Fraser <quincy@packetbucket.com>
2017-07-06 14:58:01 -0600

    Added 'nmap-service-parsing' to 'projects' subcommand.

--- commands/projects.py
+++ commands/projects.py
@@ -1,7 +1,13 @@
+import csv
+import io
+import os
+import sys
+
 import arrow
 import click
+from lxml import etree
 
-__version__ = '1.0'
+__version__ = '2.0'
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
@@ -28,3 +34,49 @@ def report_name(ctx, client_short, user_initials, report_type):
     utc = arrow.utcnow()
     click.secho('{client_short}_{report_type}_{date}_{user_initials}_v0.1.docx'.format(client_short=client_short.upper(), report_type=report_type.upper(), date=utc.to('local').format('YYYY-MM-DD'), user_initials=user_initials))
 
+@cli.command(name='nmap-service-parsing', context_settings=CONTEXT_SETTINGS, short_help='Parse a given Nmap file and output all accessable services')
+@click.option('-p', '--nmap-path', type=click.Path(exists=True, file_okay=True, dir_okay=True, readable=True, resolve_path=True, allow_dash=True), required=True)
+@click.pass_context
+def nmap_parser(ctx, nmap_path):
+    """
+    Parse a given nmap file into a nice table for Word with all given services and ports
+    """
+
+    headers = ['Host', 'Port', 'Protocol', 'Application', 'Version']
+    output = io.StringIO()
+    finalData = list()
+
+    # Check if the file given is an XML file
+    if os.path.isfile(nmap_path) and nmap_path.endswith('xml'):
+
+        tree = etree.parse(nmap_path)
+
+        root = tree.getroot()
+
+        for h in root.xpath('./host'):
+            for hostAttrib in h:
+                if hostAttrib.tag == 'address':
+                    addr = hostAttrib.attrib['addr']
+                elif hostAttrib.tag == 'ports':
+                    for ports in hostAttrib:
+                        if ports.tag == 'port':
+                            protocol = ports.attrib['protocol']
+                            portNumber = ports.attrib['portid']
+                            for service in ports:
+                                if service.tag == 'service':
+                                    application =service.attrib['name']
+                                    try:
+                                        version = service.attrib['product']
+                                    except KeyError:
+                                        version = ""
+                    data = {'Host': addr, 'Port': portNumber, 'Protocol': protocol, 'Application': application, 'Version': version}
+                    finalData.append(data)
+
+        writer = csv.DictWriter(output, headers)
+        writer.writeheader()
+        writer.writerows(finalData)
+
+        click.secho(output.getvalue())
+
+    else:
+        raise click.BadOptionUsage('You can\'t use a folder just yet.', ctx=ctx)
