c18d3b7c6724e434baabf4f37301cd6db104850a
Scott.Fraser <quincy@packetbucket.com>
2017-09-15 16:05:40 -0600

    Moved lights command from under utils to its own parent.

--- commands/utils.py
+++ commands/utils.py
@@ -7,7 +7,6 @@ import arrow
 import boto3
 import click
 import click_spinner
-from lifxlan import LifxLAN, WARM_WHITE, PINK
 
 # My Junk
 from lazyLib import lazyTools
@@ -110,71 +109,3 @@ def backup(ctx):
         with open(backup_full, 'rb') as f:
             s3.upload_fileobj(f, bucket_name, backup_filename)
     click.echo('[!] Done! \n')
-
-@cli.group('lights', help='Base command for the controlling the lights.', context_settings=CONTEXT_SETTINGS)
-@click.pass_context
-def lights(ctx):
-    """
-    Base command for controlling the lights
-    """
-
-@lights.command('out', help='Turns off all Lifx bulbs on the local network.')
-@click.pass_context
-def lights_out(ctx):
-    """
-    Turns off all Lifx bulbs on the local network.
-    """
-
-    num_lights = None
-
-    lifx = LifxLAN(num_lights, verbose=False)
-
-    # get devices
-    devices = lifx.get_lights()
-    labels = []
-    for device in devices:
-        # pprint(vars(device))
-        labels.append({'label': device.get_label(), 'ip_addr': device.get_ip_addr()})
-
-    if ctx.parent.parent.parent.params['verbose'] == True:
-        click.echo("Found Bulbs:")
-        for label in labels:
-            print('[-] Label: {}\n[->] IP Address: {}'.format(label['label'], label['ip_addr']))
-
-    lifx.set_power_all_lights("off", rapid=True)
-
-@lights.command('normal', help='Sets the bulbs to a normal color.')
-@click.pass_context
-def normal(ctx):
-    """
-    Set all the lights to normal
-    """
-
-    num_lights = None
-
-    lifx = LifxLAN(num_lights, verbose=False)
-
-    # get devices
-    devices = lifx.get_lights()
-
-    for device in devices:
-        # Set color
-        device.set_color(WARM_WHITE, rapid=True)
-
-@lights.command('pink', help='Sets the bulbs to pink.')
-@click.pass_context
-def normal(ctx):
-    """
-    Set all the lights to pink
-    """
-
-    num_lights = None
-
-    lifx = LifxLAN(num_lights, verbose=False)
-
-    # get devices
-    devices = lifx.get_lights()
-
-    for device in devices:
-        # Set color
-        device.set_color(PINK, rapid=True)
