00792902cdf83a1e72a135d8d4aeb1534fca10ae
Scott.Fraser <quincy@packetbucket.com>
2018-02-27 16:33:49 -0700

    Added info to use SetupTools and properly link to the script's base.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='lazy',
+    version='1.0',
+    description='A collection of commands to make life lazier',
+    author='Scott Fraser',
+    author_email='quincy.fraser@gmail.com',
+    url='https://github.com/radioboyQ/lazy',
+    packages=['commands', 'lazyLib'],
+    py_modules=['lazy'],
+    entry_points='''
+        [console_scripts]
+        lazy=lazy:cli
+    ''',
+     )
