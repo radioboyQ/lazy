f99ded53388ddfbb7e3ca001b8318797125a2e58
Quincy <quincy@packetbucket.com>
2019-06-18 16:22:34 -0600

    Updated hosts_in_scope to use processes for faster processing. Added hosts_in_scope_file for use with files.

--- commands/ipaddrs.py
+++ commands/ipaddrs.py
@@ -1,4 +1,6 @@
 # Standard Library
+from collections import namedtuple
+from functools import partial
 from ipaddress import (
     IPv4Address,
     IPv6Address,
@@ -7,7 +9,9 @@ from ipaddress import (
     ip_address,
     ip_network,
 )
+from multiprocessing import Pool
 from pprint import pprint
+from tabulate import tabulate
 
 # Third Party Libraries
 import click
@@ -116,7 +120,7 @@ def add_IPs(ctx, filename):
     click.echo("[*] There are {} addresses in this file.".format(count))
 
 
-@cli.command(name="host-in-scope", help="Check if a given host IP address is in scope.")
+@cli.command(name="host-in-scope-file", help="Check if a given host IP address is in scope.")
 @click.option(
     "--host",
     type=LazyCustomTypes.ipaddr,
@@ -133,7 +137,7 @@ def add_IPs(ctx, filename):
     type=click.Path(exists=True, file_okay=True, dir_okay=False, resolve_path=True),
 )
 @click.pass_context
-def add_IPs(ctx, host, scope_filename, unknown_file):
+def host_in_scope_file(ctx, host, scope_filename, unknown_file):
     """
     Check if host is in filename
     """
@@ -174,6 +178,73 @@ def add_IPs(ctx, host, scope_filename, unknown_file):
                 click.secho(
                     "[!] The address {} is *not* in scope. ".format(ip), bold=True
                 )
+
+
+@cli.command(name="host-in-scope", help="Check if a given host IP address is in scope.")
+@click.option(
+    "-h",
+    "--host",
+    type=LazyCustomTypes.ipaddr,
+    help="Host to check against scope IPs. Multiples OK",
+    multiple=True, required=True
+)
+@click.argument(
+    "scope-addresses",
+    type=click.STRING,
+    nargs=-1,
+)
+@click.option("-w", "--workers", default=4, required=False, type=click.INT, help="Number of helper processes to stand up", show_default=True)
+@click.pass_context
+def host_in_scope(ctx, host, scope_addresses, workers):
+    """
+    Check if host is in IP addresses
+    """
+    scope_addresses_list = list()
+    unknown_IP_List = list()
+
+    # Create dictionary for our output
+    ScopeOutputDict = {'In_Scope': False, "Found_In": None, "Host": None}
+
+
+    # Make sure the unknown host is an IP address or network
+    for h in host:
+        scope = lazyTools.IPTools.checkIfIP(h)
+        unknown_IP_List.append(scope)
+
+    # Ensure the scope addresses are an IP or network address
+    for s in scope_addresses:
+        scope = lazyTools.IPTools.checkIfIP(s)
+        scope_addresses_list.append(scope)
+
+    # Create an object we can pass to map later
+    func = partial(process_pool_ip_in_scope, scope_addresses_list, ScopeOutputDict)
+
+    # Use process pool to determine if unknown IP is in the scope
+    with Pool(workers) as p:
+        # We have to use a dict so it's only "one" argument
+        results = p.map(func, unknown_IP_List)
+
+    print(tabulate(results, headers="keys", tablefmt="grid"))
+
+def process_pool_ip_in_scope(scope_addresses_list, ScopeOutputDict, unknown_IP_addr):
+    """
+    Run this function in a process pool to get faster processing times
+    """
+
+    for addr in scope_addresses_list:
+        # If Scope address is a network
+        if isinstance(addr, IPv4Network) or isinstance(addr, IPv6Network):
+            ScopeOutputDict.update(In_Scope=False, Found_In=None, Host=unknown_IP_addr)
+            if unknown_IP_addr in addr.hosts():
+                ScopeOutputDict.update(In_Scope=True, Found_In=addr, Host=unknown_IP_addr)
+
+        # If scope address is a single host
+        elif isinstance(addr, IPv4Address) or isinstance(addr, IPv6Address):
+            # elif scope is a single address
+            if unknown_IP_addr == addr:
+                ScopeOutputDict.update(In_Scope=True, Found_In=addr, Host=unknown_IP_addr)
+
+    return ScopeOutputDict
 
 
 @cli.command(name="uniq-list", help="Print a list of uniq IP addresses from list ")
